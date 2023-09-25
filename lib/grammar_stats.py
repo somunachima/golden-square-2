dd11705bed5b52a8377b1074d346771f66ab385f
somunachima <somunachima@gmail.com>
2023-09-25 14:38:00 +0100

    complete challenge for grammar stats

--- lib/grammar_stats.py
+++ lib/grammar_stats.py
@@ -4,21 +4,18 @@ class GrammarStats:
     def __init__(self):
         self.good = 0
         self.bad = 0
-        self.calculate = 0
   
     def check(self, text):
         if text == "":
             raise Exception("Must include text")
         elif (text[0] == text[0].upper()) and (text[-1] in "?!."):
+            self.good += 1
             return True
         else:
+            self.bad += 1
             return False
   
-    def percentage_good(self, text):
-        if self.check(text) == True:
-            self.good += 1
-        else:
-            self.bad += 1
-        self.calculate = (self.good / (self.bad + self.good)) * 100
-        start_percent = math.ceil(self.calculate)
-        return start_percent
+    def percentage_good(self):
+        # calculating percentage and using math.ceil to round the float
+        percent = math.ceil((self.good / (self.bad + self.good)) * 100)
+        return percent
--- tests/test_grammar_stats.py
+++ tests/test_grammar_stats.py
@@ -1,13 +1,49 @@
 import pytest
 from lib.grammar_stats import GrammarStats
 
+def test_for_zero_good_stats():
+    grammer_stats = GrammarStats()
+    assert grammer_stats.good == 0
+
+def test_for_zero_bad_stats():
+    grammer_stats = GrammarStats()
+    assert grammer_stats.good == 0
+
+def test_empty_grammarStats():
+    grammar_stats = GrammarStats()
+    with pytest.raises(Exception) as e:
+        grammar_stats.check("")
+    assert str(e.value) == "Must include text"
+
 def test_true_exlamation_grammarStats():
     grammar_stats = GrammarStats()
-    assert grammar_stats.check("Wicked!") == True
+    grammar_stats.check("Wicked!") == True
+
+def test_true_exlamation_and_count_for_good_grammarStats():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Wicked!") == True
+    assert grammar_stats.good == 1
+
+def test_true_exlamation_two_grammarStats():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Wicked!") == True
+    grammar_stats.check("Wicked?") == True
+    assert grammar_stats.good == 2
 
 def test_false_no_punctuation_grammarStats():
     grammar_stats = GrammarStats()
-    assert grammar_stats.check("Wicked") == False
+    grammar_stats.check("Wicked") == False
+
+def test_false_no_punctuation_and_count_one_bad_grammarStats():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Wicked") == False
+    assert grammar_stats.bad == 1
+
+def test_false_no_punctuation_and_count_two_bad_grammarStats():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Wicked") == False
+    grammar_stats.check("wicked") == False
+    assert grammar_stats.bad == 2
 
 def test_false_exclamation_grammarStats():
     grammar_stats = GrammarStats()
@@ -29,28 +65,55 @@ def test_false_dot_grammarStats():
     grammar_stats = GrammarStats()
     assert grammar_stats.check("wicked.") == False
 
-def test_empty_grammarStats():
+def test_two_bad_and_one_good_grammarStats():
     grammar_stats = GrammarStats()
-    with pytest.raises(Exception) as e:
-        grammar_stats.check("")
-    assert str(e.value) == "Must include text"
+    grammar_stats.check("Wicked!") == True
+    grammar_stats.check("Wicked") == False
+    grammar_stats.check("wicked") == False
+    assert grammar_stats.bad == 2
+    assert grammar_stats.good == 1
 
-def test_percentage_bad():
+def test_percent_good_when_two_bad_and_one_good():
     grammar_stats = GrammarStats()
-    assert grammar_stats.percentage_good("Wicked") == 0
+    grammar_stats.check("Wicked!") == True
+    grammar_stats.check("Wicked") == False
+    grammar_stats.check("wicked") == False
+    grammar_stats.bad == 2
+    grammar_stats.good == 1
+    assert grammar_stats.percentage_good() == 34
 
-def test_percentage_good():
+def test_percent_good_when_two_bad_and_two_good():
     grammar_stats = GrammarStats()
-    assert grammar_stats.percentage_good("Wicked.") == 100
+    grammar_stats.check("Wicked!") == True
+    grammar_stats.check("Wicked?") == True
+    grammar_stats.check("Wicked") == False
+    grammar_stats.check("wicked") == False
+    grammar_stats.bad == 2
+    grammar_stats.good == 2
+    assert grammar_stats.percentage_good() == 50
 
-# def test_mix_percentage_good_and_bad():
-#     grammar_stats = GrammarStats()
-#     assert grammar_stats.percentage_good("Wicked.") == 100
-#     assert grammar_stats.percentage_good("Wicked?") == 100
-#     assert grammar_stats.percentage_good("Wicked!") == 100
-#     assert grammar_stats.percentage_good("Wicked.") == 75
-#     assert grammar_stats.percentage_good("Wicked?") == 60
-#     assert grammar_stats.percentage_good("Wicked!") == 50
+def test_percent_good_when_three_bad_and_two_good():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Wicked!") == True
+    grammar_stats.check("Wicked?") == True
+    grammar_stats.check("Wicked") == False
+    grammar_stats.check("wicked") == False
+    grammar_stats.check("wicked.") == False
+    grammar_stats.bad == 3
+    grammar_stats.good == 2
+    assert grammar_stats.percentage_good() == 40
+
+def test_percent_good_when_two_bad_and_three_good():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Wicked!") == True
+    grammar_stats.check("Wicked?") == True
+    grammar_stats.check("Wicked.") == True
+    grammar_stats.check("wicked") == False
+    grammar_stats.check("wicked.") == False
+    grammar_stats.bad == 2
+    grammar_stats.good == 3
+    assert grammar_stats.percentage_good() == 60
+
 
  # Returns:
         #   int: the percentage of texts checked so far that passed the check
